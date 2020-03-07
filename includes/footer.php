1fa2f01d3584a4d4a9f0f8c79558c52dbcc44c18
Prashant Agnihotri <49424915+Prashant2108@users.noreply.github.com>
2020-03-06 20:56:36 -0800

    initial upload

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/" target="_blank"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://twitter.com/home" target="_blank"><span>Twitter</span></a></li>
+				<li><a class="flickr" href="https://www.flickr.com/" target="_blank"><span>Flickr</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2017 TMS . All Rights Reserved </p>
+	</div>
+</div>
